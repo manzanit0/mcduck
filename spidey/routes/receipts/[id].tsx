73d0762b075a4b56b39dc8ecf730b776dcfe5221
Javier García <unico.javier@gmail.com>
2024-10-18 15:24:01 +0200

    feat: support multiple content types on receipt view

--- spidey/routes/receipts/[id].tsx
+++ spidey/routes/receipts/[id].tsx
@@ -26,24 +26,43 @@ export default async function Single(_: Request, ctx: RouteContext<AuthState>) {
     { id: BigInt(ctx.params.id) },
     {
       headers: { authorization: `Bearer ${ctx.state.authToken}` },
-    }
+    },
   );
 
   const receipt = mapReceiptsToSerializable([res.receipt!])[0];
   const encoded = encodeBase64(res.receipt!.file);
 
+  let receiptView = (
+    <img
+      class="object-contain"
+      src={`data:image/png;base64, ${encoded}`}
+      alt="Receipt image"
+    />
+  );
+
+  const contentType = detectContentType(res.receipt!.file);
+  if (contentType === "application/pdf") {
+    receiptView = (
+      <embed
+        class=" overflow-visible"
+        src={`data:application/pdf;base64, ${encoded}`}
+        type="application/pdf"
+        frameborder="0"
+        scrolling="auto"
+        height="123%"
+        width="100%"
+      />
+    );
+  }
+
   return (
-    <div class="m-6">
+    <div class="m-6 h-screen">
       <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
         Receipt #{ctx.params.id}
       </h2>
       <div class="mt-10 grid grid-cols-3 gap-4 items-center">
-        <div class="col-span-1">
-          <img
-            class="object-contain"
-            src={`data:image/png;base64, ${encoded}`}
-            alt="Receipt image"
-          />
+        <div class="col-span-1 h-full">
+          {receiptView}
         </div>
         <div class="col-span-2 p-5">
           <ReceiptForm receipt={receipt} url={url} />
@@ -62,4 +81,22 @@ export default async function Single(_: Request, ctx: RouteContext<AuthState>) {
       </div>
     </div>
   );
+}
+
+function detectContentType(data: Uint8Array): string {
+  const inspected = Deno.inspect(data).replaceAll(" ", "").replaceAll("\n", "");
+
+  if (inspected.includes("[255,216,255")) {
+    return "image/jpeg";
+  } else if (
+    inspected.includes("[137,80,78,71,13,10,26,10")
+  ) {
+    return "image/png";
+  } else if (inspected.includes("[71,73,70,56")) {
+    return "image/gif";
+  } else if (inspected.includes("[37,80,68,70")) {
+    return "application/pdf";
+  } else {
+    return "application/octet-stream"; // Default binary data type
+  }
 }
